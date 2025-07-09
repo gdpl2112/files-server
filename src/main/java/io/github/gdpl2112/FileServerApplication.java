a4dcc0079565c64f3713a3882b0dbb6238c57bb3
Kloping <3474006766@qq.com>
2025-07-09 13:57:31 +0800

    update

--- src/main/java/io/github/gdpl2112/FileServerApplication.java
+++ src/main/java/io/github/gdpl2112/FileServerApplication.java
@@ -1,13 +1,20 @@
 package io.github.gdpl2112;
 
+import io.github.kloping.file.FileUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import java.io.File;
+import java.lang.management.ManagementFactory;
 
 @SpringBootApplication
 @ServletComponentScan
 public class FileServerApplication {
     public static void main(String[] args) {
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        String pid = name.split("@")[0];
+        FileUtils.putStringInFile(pid, new File("./fs.pid"));
         SpringApplication.run(FileServerApplication.class, args);
     }
 }
