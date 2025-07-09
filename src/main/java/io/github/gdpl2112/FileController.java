bd9ba6d6f9f5d5d4c6e481cb97d4168d9050b26c
Kloping <3474006766@qq.com>
2025-07-09 13:04:52 +0800

    update

---
+++ src/main/java/io/github/gdpl2112/FileController.java
@@ -0,0 +1,69 @@
+package io.github.gdpl2112;
+
+import io.github.kloping.date.DateUtils;
+import io.github.kloping.file.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RestController
+public class FileController {
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Value("${server.host}")
+    private String host;
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(
+            @RequestParam(name = "file") MultipartFile multipartFile
+            , @RequestParam(name = "path", required = false) String path
+            , @RequestParam(name = "name", required = false) String name
+            , @RequestParam(name = "suffix", required = false, defaultValue = "jpg") String suffix
+    ) {
+        if (multipartFile.isEmpty()) return ResponseEntity.badRequest().body("文件为空");
+        try {
+            if (io.github.kloping.judge.Judge.isEmpty(path)) {
+                path = DateUtils.getYear() + "_" + DateUtils.getMonth();
+            }
+            File dir = new File(uploadDir, path);
+            if (io.github.kloping.judge.Judge.isEmpty(name)) {
+                name = DateUtils.getDay() + "-" + UUID.randomUUID() + "." + suffix;
+            }
+            File dist = new File(dir.getPath(), name);
+            FileUtils.testFile(dist);
+            FileUtils.writeBytesToFile(multipartFile.getBytes(), dist);
+            String outname = dist.getPath().replaceAll("\\\\", "/");
+            outname = outname.replace(uploadDir, "");
+            return ResponseEntity.ok(host + ":" + port + outname);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("上传失败: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/download/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+        Path file = Paths.get(uploadDir).resolve(filename);
+        try {
+            Resource resource = new UrlResource(file.toUri());
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+        } catch (IOException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
