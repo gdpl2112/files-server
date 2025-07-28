b19b121150d19397a5ccef2c8eb5526d7ff93249
Kloping <3474006766@qq.com>
2025-07-28 21:12:24 +0800

    feat(script): 优化运行脚本，添加参数提示、默认跳过编译及编译前倒计时功能

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
-if [ ${1} == "false" ]; then
-  echo "Skip compilation"
+#!/bin/bash
+
+if [ -z "${1}" ]; then
+    echo "[bash] No Parameters,Default Skip Compilation."
+elif [ "${1}" = "false" ]; then
+    echo "[bash] Skip Compilation."
 else
-  rm -r libs
-  mvn clean dependency:copy-dependencies -DoutputDirectory=libs compile
+    echo "[bash] Start Clean And Copy-dependencies Compilation."
+    echo "[bash] Delay 3s after rm -r libs"
+    echo "[bash] 3s"
+    sleep 1s
+    echo "[bash] 2s"
+    sleep 1s
+    echo "[bash] 1s"
+    sleep 1s
+    rm -r libs
+    mvn clean dependency:copy-dependencies -DoutputDirectory=libs compile
 fi
 
 java -Dfile.encoding=UTF-8 -classpath "./target/classes:./libs/*" io.github.gdpl2112.FileServerApplication
