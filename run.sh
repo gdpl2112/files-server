76ef53d0162054a98ddf67ccf9dc79cd9c7faf46
Kloping <3474006766@qq.com>
2026-02-20 10:34:58 +0800

    feat(run): 添加仅编译选项并优化脚本执行流程
    - 新增 cc 参数用于仅执行编译操作
    - 添加 git pull 命令确保代码同步
    - 执行 mvn clean compile 进行项目编译
    - 在脚本末尾添加退出提示信息
    - 优化参数判断逻辑和执行流程

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ if [ -z "${1}" ]; then
     echo "[bash] No Parameters,Default Skip Compilation."
 elif [ "${1}" = "false" ]; then
     echo "[bash] Skip Compilation."
+elif [ "${1}" = "cc" ]; then
+    echo "[bash] Only Compilation."
+    git pull
+    mvn clean compile
 else
     echo "[bash] Start Clean And Copy-dependencies Compilation."
     echo "[bash] Delay 3s after rm -r libs"
@@ -18,3 +22,5 @@ else
 fi
 
 java -Dfile.encoding=UTF-8 -Xmx200m -Xms100m -XX:MaxHeapSize=200m -XX:+UseG1GC -classpath "./target/classes:./libs/*" io.github.gdpl2112.FileServerApplication
+
+echo "[bash] Exit."
